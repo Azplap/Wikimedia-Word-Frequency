dcc24892a972c50e85fade41735ea2d366385b00
Austin <austinpeng97@gmail.com>
2021-06-20 20:05:37 -0500

    Done?

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wikimedia Word Frequency
- 
+
+I used a hash table to store the frequency of words. The big O runtime should be O(n).
+
+Run freq_counter.py to return the desired output to the console.
--- freq_counter.py
+++ freq_counter.py
@@ -5,18 +5,26 @@ Input: text file
 Output: returns list in console
 
 Possible improvements:
-- workaround weird symbols and spelling mistakes?
+- workaround weird symbols and capitalization?
 - improve big O ?
-- display results outside of console
+- display results w/ matplotlib
 
 """
 from collections import defaultdict
+from pprint import pprint
 
 # reads in txt file + converts to list of strings
 def read_txt_file(file):
     with open(file, errors="ignore") as f:
         content = f.read().split()
-    return content
+    return lowercase(content)
+
+# goes through the list and makes everything lowercase
+def lowercase(words):
+    new=[]
+    for word in words:
+        new.append(word.lower())
+    return new
 
 # counts string freq w/ hash table
 def count_freq(words):
@@ -27,8 +35,8 @@ def count_freq(words):
 
 
 if __name__ == "__main__":
-    file_name = 'test_docs/description.txt'
+    file_name = 'test_txts/long.txt'
     words = read_txt_file(file_name)
     dictionary = count_freq(words)
     sorted_dictionary = sorted(dictionary.items(), key=lambda kv: kv[1], reverse=True)
-    print(sorted_dictionary)
+    pprint(sorted_dictionary)
